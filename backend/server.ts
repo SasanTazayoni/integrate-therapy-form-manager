512430864db572bb305e756ec8348f162e590787
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-07-20 08:48:07 +0100

    Update server port to 3001 and tweak log message

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import app from "./index";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server listening on http://localhost:${PORT}`);
 });
