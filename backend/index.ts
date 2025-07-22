5db5caf12842cd3f3b75ee488b89f25e8688ff42
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-07-22 16:28:12 +0100

    Add sendFormToken route to Express app

--- backend/index.ts
+++ backend/index.ts
@@ -3,15 +3,16 @@ import cors from "cors";
 import dotenv from "dotenv";
 import clientRoutes from "./routes/clients";
 import formRoutes from "./routes/forms";
+import sendFormTokenRoute from "./routes/sendFormToken";
 
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 app.use("/clients", clientRoutes);
 app.use("/forms", formRoutes);
+app.use(sendFormTokenRoute);
 
 export default app;
