a1923ad412f146b2f5c1b2a1bea948d4b70c5f3e
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-07-27 01:49:32 +0100

    Update import names after file name changes

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import clientRoutes from "./routes/clients";
-import formRoutes from "./routes/forms";
+import clientRoutes from "./routes/clientsBackend";
+import formRoutes from "./routes/formsBackend";
 
 dotenv.config();
 
