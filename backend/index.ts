86750522dd47ed80c5dfe774cca954b0655662fa
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-07-28 12:30:37 +0100

    Install Helmet middleware for enhanced HTTP security headers and update dev dependencies

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import clientRoutes from "./routes/clientsBackend";
 import formRoutes from "./routes/formsBackend";
 
 dotenv.config();
 
 const app = express();
+
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
