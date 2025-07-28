0589d2055c47ef8c435d6ab05a81e28111c25cf8
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-07-28 12:52:03 +0100

    Add CORS middleware for enhanced security and cross-origin support

--- backend/index.ts
+++ backend/index.ts
@@ -4,13 +4,19 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import clientRoutes from "./routes/clientsBackend";
 import formRoutes from "./routes/formsBackend";
+import { getFrontendBaseUrl } from "./utils/getFrontendBaseUrl";
 
 dotenv.config();
 
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: getFrontendBaseUrl(),
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/clients", clientRoutes);
