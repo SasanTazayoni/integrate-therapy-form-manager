dd5f97ea0ffde53d9180f84d552e7e09935e92e5
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-07-29 23:10:20 +0100

    Nest routes under root with shared RouteError boundary and add 404 route

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,30 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import YSQ from "./pages/YSQ";
+import Dashboard from "./pages/Dashboard";
 import SMI from "./pages/SMI";
+import YSQ from "./pages/YSQ";
 import BECKS from "./pages/BECKS";
 import BURNS from "./pages/BURNS";
-import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFoundPage";
-
 import { SMIAction } from "./actions/SMIAction";
 import { YSQAction } from "./actions/YSQAction";
 import { BECKSAction } from "./actions/BECKSAction";
 import { BURNSAction } from "./actions/BURNSAction";
 
+import RouteError from "./components/RouteError";
+
 const router = createBrowserRouter(
   [
     {
       path: "/",
-      element: <Dashboard />,
-    },
-    {
-      path: "/SMI/:token",
-      element: <SMI />,
-      action: SMIAction,
-    },
-    {
-      path: "/YSQ/:token",
-      element: <YSQ />,
-      action: YSQAction,
-    },
-    {
-      path: "/BECKS/:token",
-      element: <BECKS />,
-      action: BECKSAction,
-    },
-    {
-      path: "/BURNS/:token",
-      element: <BURNS />,
-      action: BURNSAction,
-    },
-    {
-      path: "*",
-      element: <NotFound />,
+      errorElement: <RouteError />,
+      children: [
+        { index: true, element: <Dashboard /> },
+        { path: "SMI/:token", element: <SMI />, action: SMIAction },
+        { path: "YSQ/:token", element: <YSQ />, action: YSQAction },
+        { path: "BECKS/:token", element: <BECKS />, action: BECKSAction },
+        { path: "BURNS/:token", element: <BURNS />, action: BURNSAction },
+        { path: "*", element: <NotFound /> },
+      ],
     },
   ],
   {
