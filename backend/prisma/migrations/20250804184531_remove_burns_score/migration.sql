73d7ae8408b4cebb921f5e1beaa11f9c666f77ac
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-04 20:53:32 +0100

    Update schema and add BURNS score migration

---
+++ backend/prisma/migrations/20250804184531_remove_burns_score/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `burns_score` on the `Form` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Form" DROP COLUMN "burns_score";
