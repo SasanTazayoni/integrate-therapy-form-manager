81993be0ab9716eb41c3a5f5ab6cb3251466d650
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:29:51 +0100

    Add Unrelenting Standards questions

---
+++ src/data/YSQUnrelentingStandards.ts
@@ -0,0 +1,91 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQUnrelentingStandards: Item[] = [
+  {
+    id: 152,
+    prompt:
+      "I must be the best at most of what I do: I can’t accept second best",
+    options: OPTIONS,
+  },
+  {
+    id: 153,
+    prompt: "I strive to keep almost everything in perfect order",
+    options: OPTIONS,
+  },
+  {
+    id: 154,
+    prompt: "I must look my best most of the time",
+    options: OPTIONS,
+  },
+  {
+    id: 155,
+    prompt: "I try to do my best: I can’t settle for ‘good enough’",
+    options: OPTIONS,
+  },
+  {
+    id: 156,
+    prompt:
+      "I have so much to accomplish that there is almost no time to really relax",
+    options: OPTIONS,
+  },
+  {
+    id: 157,
+    prompt: "Almost nothing I do is quite good enough, I can always do better",
+    options: OPTIONS,
+  },
+  {
+    id: 158,
+    prompt: "I must meet all my responsibilities",
+    options: OPTIONS,
+  },
+  {
+    id: 159,
+    prompt:
+      "I feel there is constant pressure for me to achieve and get things done",
+    options: OPTIONS,
+  },
+  {
+    id: 160,
+    prompt: "My relationships suffer because I push myself so hard",
+    options: OPTIONS,
+  },
+  {
+    id: 161,
+    prompt:
+      "My health is suffering because I put myself under so much pressure to do well",
+    options: OPTIONS,
+  },
+  {
+    id: 162,
+    prompt: "I often sacrifice pleasure and happiness to meet my own standards",
+    options: OPTIONS,
+  },
+  {
+    id: 163,
+    prompt: "When I make a mistake, I deserve strong criticism",
+    options: OPTIONS,
+  },
+  {
+    id: 164,
+    prompt:
+      "I can’t let myself off the hook easily or make excuses for my mistakes",
+    options: OPTIONS,
+  },
+  {
+    id: 165,
+    prompt: "I’m a very competitive person",
+    options: OPTIONS,
+  },
+  {
+    id: 166,
+    prompt: "I put a good deal of emphasis on money or status",
+    options: OPTIONS,
+  },
+  {
+    id: 167,
+    prompt: "I always have to be Number One, in terms of my performance.",
+    options: OPTIONS,
+  },
+];
+
+export default YSQUnrelentingStandards;
