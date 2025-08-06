7a3759b1c5af042ed426899511ac8640229d1033
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:26:39 +0100

    Add Defectiveness questions

---
+++ src/data/YSQDefectiveness.ts
@@ -0,0 +1,86 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQDefectiveness: Item[] = [
+  {
+    id: 54,
+    prompt: "No man/woman I desire could love me once he/she saw my defects",
+    options: OPTIONS,
+  },
+  {
+    id: 55,
+    prompt:
+      "No one I desire would want to stay close to me if he/she knew the real me",
+    options: OPTIONS,
+  },
+  {
+    id: 56,
+    prompt: "I am inherently flawed and defective",
+    options: OPTIONS,
+  },
+  {
+    id: 57,
+    prompt:
+      "No matter how hard I try, I feel that I won’t be able to get a significant man/woman to respect me or feel that I am worthwhile",
+    options: OPTIONS,
+  },
+  {
+    id: 58,
+    prompt: "I’m unworthy of the love, attention, and respect of others",
+    options: OPTIONS,
+  },
+  {
+    id: 59,
+    prompt: "I feel that I’m not lovable",
+    options: OPTIONS,
+  },
+  {
+    id: 60,
+    prompt:
+      "I am too unacceptable in very basic ways to reveal myself to other people",
+    options: OPTIONS,
+  },
+  {
+    id: 61,
+    prompt: "If others found out about my basic defects, I could not face them",
+    options: OPTIONS,
+  },
+  {
+    id: 62,
+    prompt: "When people like me, I feel I am fooling them",
+    options: OPTIONS,
+  },
+  {
+    id: 63,
+    prompt:
+      "I often find myself drawn to people who are very critical of me or reject me",
+    options: OPTIONS,
+  },
+  {
+    id: 64,
+    prompt:
+      "I have inner secrets that I don’t want people close to me to find out",
+    options: OPTIONS,
+  },
+  {
+    id: 65,
+    prompt: "It is my fault that my parent(s) could not love me enough",
+    options: OPTIONS,
+  },
+  {
+    id: 66,
+    prompt: "I don’t feel I let people know the real me",
+    options: OPTIONS,
+  },
+  {
+    id: 67,
+    prompt: "One of my greatest fears is that my defects will be exposed",
+    options: OPTIONS,
+  },
+  {
+    id: 68,
+    prompt: "I cannot understand how anyone could love me",
+    options: OPTIONS,
+  },
+];
+
+export default YSQDefectiveness;
