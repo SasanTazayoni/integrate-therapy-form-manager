932943a3de337cd1a5df0afba794b09b39fea5d7
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:25:12 +0100

    Add Mistrust/Abuse questions

---
+++ src/data/YSQMistrustAbuse.ts
@@ -0,0 +1,100 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQMistrustAbuse: Item[] = [
+  {
+    id: 27,
+    prompt: "I feel that people will take advantage of me",
+    options: OPTIONS,
+  },
+  {
+    id: 28,
+    prompt: "I often feel that I have to protect myself from other people",
+    options: OPTIONS,
+  },
+  {
+    id: 29,
+    prompt:
+      "I feel that I cannot let my guard down in the presence of other people, or else they will intentionally hurt me",
+    options: OPTIONS,
+  },
+  {
+    id: 30,
+    prompt:
+      "If someone acts nicely towards me, I assume that he/she must be after something",
+    options: OPTIONS,
+  },
+  {
+    id: 31,
+    prompt: "It is only a matter of time before someone betrays me",
+    options: OPTIONS,
+  },
+  {
+    id: 32,
+    prompt: "Most people only think about themselves",
+    options: OPTIONS,
+  },
+  {
+    id: 33,
+    prompt: "I have a great deal of difficulty trusting people",
+    options: OPTIONS,
+  },
+  {
+    id: 34,
+    prompt: "I am quite suspicious of other people’s motives",
+    options: OPTIONS,
+  },
+  {
+    id: 35,
+    prompt:
+      "Other people are rarely honest, they are usually not what they appear",
+    options: OPTIONS,
+  },
+  {
+    id: 36,
+    prompt: "I’m usually on the lookout for people’s ulterior motives",
+    options: OPTIONS,
+  },
+  {
+    id: 37,
+    prompt:
+      "If I think someone is out to hurt me, I try to hurt him or her first",
+    options: OPTIONS,
+  },
+  {
+    id: 38,
+    prompt:
+      "People usually have to prove themselves to me before I can trust them",
+    options: OPTIONS,
+  },
+  {
+    id: 39,
+    prompt:
+      "I set up ‘tests’ for other people, to see if they are telling me the truth and are well-intentioned",
+    options: OPTIONS,
+  },
+  {
+    id: 40,
+    prompt: "I subscribe to the belief: ‘Control or be controlled.’",
+    options: OPTIONS,
+  },
+  {
+    id: 41,
+    prompt:
+      "I get angry when I think about the ways I have been mistreated by other people during my life",
+    options: OPTIONS,
+  },
+  {
+    id: 42,
+    prompt:
+      "During my life, those close to me have taken advantage of me or used me for their own purpose",
+    options: OPTIONS,
+  },
+  {
+    id: 43,
+    prompt:
+      "I have been physically, emotionally, or sexually abused by important people in my life",
+    options: OPTIONS,
+  },
+];
+
+export default YSQMistrustAbuse;
