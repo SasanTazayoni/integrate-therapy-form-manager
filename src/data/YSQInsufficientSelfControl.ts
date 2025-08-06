68489a0b010ac321ac4005bed96b612fe511f757
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:31:13 +0100

    Add Insufficient Self Control questions

---
+++ src/data/YSQInsufficientSelfControl.ts
@@ -0,0 +1,89 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQInsufficientSelfControl: Item[] = [
+  {
+    id: 179,
+    prompt:
+      "I have great difficulty getting myself to stop drinking, smoking, overeating, or other problem behaviours.",
+    options: OPTIONS,
+  },
+  {
+    id: 180,
+    prompt:
+      "I can’t seem to discipline myself to complete routine or boring tasks",
+    options: OPTIONS,
+  },
+  {
+    id: 181,
+    prompt:
+      "Often I allow myself to carry through on impulses and express emotions that get me into trouble or hurt other people",
+    options: OPTIONS,
+  },
+  {
+    id: 182,
+    prompt: "If I can’t reach a goal, I become easily frustrated and give up",
+    options: OPTIONS,
+  },
+  {
+    id: 183,
+    prompt:
+      "I have a very difficult time sacrificing immediate gratification to achieve a long-range goal",
+    options: OPTIONS,
+  },
+  {
+    id: 184,
+    prompt:
+      "It often happens that, once I start to feel angry, I just can’t control it",
+    options: OPTIONS,
+  },
+  {
+    id: 185,
+    prompt: "I tend to overdo things, even though I know they are bad for me",
+    options: OPTIONS,
+  },
+  {
+    id: 186,
+    prompt: "I get bored very easily",
+    options: OPTIONS,
+  },
+  {
+    id: 187,
+    prompt:
+      "When tasks become difficult, I usually cannot persevere and complete them",
+    options: OPTIONS,
+  },
+  {
+    id: 188,
+    prompt: "I can’t concentrate on anything for too long",
+    options: OPTIONS,
+  },
+  {
+    id: 189,
+    prompt:
+      "I can’t force myself to do things I don’t enjoy, even when I know it’s for my own good",
+    options: OPTIONS,
+  },
+  {
+    id: 190,
+    prompt: "I lose my temper at the slightest offence",
+    options: OPTIONS,
+  },
+  {
+    id: 191,
+    prompt: "I have rarely been able to stick to resolutions",
+    options: OPTIONS,
+  },
+  {
+    id: 192,
+    prompt:
+      "I can almost never hold back from showing people how I really feel, no matter what the cost may be",
+    options: OPTIONS,
+  },
+  {
+    id: 193,
+    prompt: "I often do things impulsively that I later regret",
+    options: OPTIONS,
+  },
+];
+
+export default YSQInsufficientSelfControl;
