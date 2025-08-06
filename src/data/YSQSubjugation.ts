6192b9476f7188b59708339842a001cb063460f9
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:28:37 +0100

    Add Subjugation questions

---
+++ src/data/YSQSubjugation.ts
@@ -0,0 +1,63 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQSubjugation: Item[] = [
+  {
+    id: 116,
+    prompt:
+      "I let other people have their way, because I fear the consequences",
+    options: OPTIONS,
+  },
+  {
+    id: 117,
+    prompt: "I believe that if I do what I want, I’m only asking for trouble",
+    options: OPTIONS,
+  },
+  {
+    id: 118,
+    prompt:
+      "I feel that I have no choice but to give in to other people’s wishes, or else they will retaliate or reject me in some way",
+    options: OPTIONS,
+  },
+  {
+    id: 119,
+    prompt: "In relationships, I let the other person have the upper hand",
+    options: OPTIONS,
+  },
+  {
+    id: 120,
+    prompt:
+      "I’ve always let others make choices for me, so I really don’t know what I want for myself",
+    options: OPTIONS,
+  },
+  {
+    id: 121,
+    prompt: "I feel the major decisions in my life were not really my own",
+    options: OPTIONS,
+  },
+  {
+    id: 122,
+    prompt:
+      "I worry a lot about pleasing other people, so they won’t reject me",
+    options: OPTIONS,
+  },
+  {
+    id: 123,
+    prompt:
+      "I have a lot of trouble demanding that my rights be respected and that my feelings be taken into account",
+    options: OPTIONS,
+  },
+  {
+    id: 124,
+    prompt:
+      "I get back at people in little ways instead of showing my anger directly",
+    options: OPTIONS,
+  },
+  {
+    id: 125,
+    prompt:
+      "I will go to much greater lengths than most people to avoid confrontations",
+    options: OPTIONS,
+  },
+];
+
+export default YSQSubjugation;
