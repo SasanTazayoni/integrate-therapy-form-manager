f16d652be0160a40d7fbd73ca62bcfda24b116df
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:31:56 +0100

    Add Negativity/Pessimism questions

---
+++ src/data/YSQNegativityPessimism.ts
@@ -0,0 +1,68 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQNegativityPessimism: Item[] = [
+  {
+    id: 208,
+    prompt:
+      "Even when things seem to be going well, I feel that it is only temporary",
+    options: OPTIONS,
+  },
+  {
+    id: 209,
+    prompt:
+      "If something good happens, I worry that something bad is likely to follow",
+    options: OPTIONS,
+  },
+  {
+    id: 210,
+    prompt: "You can't be too careful; something will almost always go wrong",
+    options: OPTIONS,
+  },
+  {
+    id: 211,
+    prompt:
+      "No matter how hard I work, I worry that I could be wiped out financially",
+    options: OPTIONS,
+  },
+  {
+    id: 212,
+    prompt: "I worry that a wrong decision could lead to disaster.",
+    options: OPTIONS,
+  },
+  {
+    id: 213,
+    prompt:
+      "I often obsess over minor decisions, because the consequences of making a mistake seem so serious",
+    options: OPTIONS,
+  },
+  {
+    id: 214,
+    prompt:
+      "I feel better assuming that things will not work out for me, so that I don’t feel disappointed if things go wrong",
+    options: OPTIONS,
+  },
+  {
+    id: 215,
+    prompt:
+      "I focus more on the negative aspects of life and of events than on the positive",
+    options: OPTIONS,
+  },
+  {
+    id: 216,
+    prompt: "I tend to be pessimistic",
+    options: OPTIONS,
+  },
+  {
+    id: 217,
+    prompt: "People close to me consider me a worrier",
+    options: OPTIONS,
+  },
+  {
+    id: 218,
+    prompt:
+      "If people get too enthusiastic about something, I become uncomfortable and feel like warning them of what could go wrong",
+    options: OPTIONS,
+  },
+];
+
+export default YSQNegativityPessimism;
