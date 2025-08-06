f1ca6b57f0cb6512cf6fee55704870345754f50e
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:29:24 +0100

    Add Emotional Inhibition questions

---
+++ src/data/YSQEmotionalInhibition.ts
@@ -0,0 +1,55 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQEmotionalInhibition: Item[] = [
+  {
+    id: 143,
+    prompt: "I worry about losing control of my actions",
+    options: OPTIONS,
+  },
+  {
+    id: 144,
+    prompt:
+      "I worry that I might seriously harm someone physically or emotionally, if my anger gets out of control",
+    options: OPTIONS,
+  },
+  {
+    id: 145,
+    prompt:
+      "I feel that I must control my emotions and impulses, or something bad is likely to happen",
+    options: OPTIONS,
+  },
+  {
+    id: 146,
+    prompt:
+      "A lot of anger and resentment build up inside of me that I don’t express",
+    options: OPTIONS,
+  },
+  {
+    id: 147,
+    prompt:
+      "I am too self-conscious to show positive feelings to others (e.g. affection, showing I care)",
+    options: OPTIONS,
+  },
+  {
+    id: 148,
+    prompt: "I find it embarrassing to express my feelings to others",
+    options: OPTIONS,
+  },
+  {
+    id: 149,
+    prompt: "I find it hard to be warm and spontaneous",
+    options: OPTIONS,
+  },
+  {
+    id: 150,
+    prompt: "I control myself so much that people think I am unemotional",
+    options: OPTIONS,
+  },
+  {
+    id: 151,
+    prompt: "People see me as uptight emotionally",
+    options: OPTIONS,
+  },
+];
+
+export default YSQEmotionalInhibition;
