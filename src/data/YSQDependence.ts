9702cb44d924b3ebd63dbe92751890974adda69c
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:27:31 +0100

    Add Dependence questions

---
+++ src/data/YSQDependence.ts
@@ -0,0 +1,88 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQDependence: Item[] = [
+  {
+    id: 78,
+    prompt: "I do not feel capable of getting by on my own in everyday life",
+    options: OPTIONS,
+  },
+  {
+    id: 79,
+    prompt: "I need other people to help me get by",
+    options: OPTIONS,
+  },
+  {
+    id: 80,
+    prompt: "I do not feel I can cope well by myself",
+    options: OPTIONS,
+  },
+  {
+    id: 81,
+    prompt:
+      "I believe that other people can take care of me better than I can take care of myself",
+    options: OPTIONS,
+  },
+  {
+    id: 82,
+    prompt:
+      "I have trouble tackling new tasks outside of work, unless I have someone to guide me",
+    options: OPTIONS,
+  },
+  {
+    id: 83,
+    prompt:
+      "I think of myself as a dependent person when it comes to everyday functioning",
+    options: OPTIONS,
+  },
+  {
+    id: 84,
+    prompt: "I screw up everything I try, even outside of work (or school)",
+    options: OPTIONS,
+  },
+  {
+    id: 85,
+    prompt: "I’m inept in most areas of life",
+    options: OPTIONS,
+  },
+  {
+    id: 86,
+    prompt:
+      "If I trust my own judgement in everyday situations, I’ll make the wrong decision",
+    options: OPTIONS,
+  },
+  {
+    id: 87,
+    prompt: "I lack common sense",
+    options: OPTIONS,
+  },
+  {
+    id: 88,
+    prompt: "My judgement cannot be relied on in everyday situations",
+    options: OPTIONS,
+  },
+  {
+    id: 89,
+    prompt:
+      "I don’t feel confident about my ability to solve everyday problems that come up",
+    options: OPTIONS,
+  },
+  {
+    id: 90,
+    prompt:
+      "I feel that I need someone I can rely on to give me advice about practical issues",
+    options: OPTIONS,
+  },
+  {
+    id: 91,
+    prompt:
+      "I feel more like a child than an adult when it comes to handling everyday responsibilities",
+    options: OPTIONS,
+  },
+  {
+    id: 92,
+    prompt: "I find the responsibilities of everyday life overwhelming",
+    options: OPTIONS,
+  },
+];
+
+export default YSQDependence;
