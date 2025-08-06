19c18d8db8b9f7fb33b3af999c80ad796b056eb6
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:24:00 +0100

    Add Emotional Deprivation questions

---
+++ src/data/YSQEmotionalDeprivation.ts
@@ -0,0 +1,57 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQEmotionalDeprivation: Item[] = [
+  {
+    id: 1,
+    prompt: "People have not been there to meet my emotional needs",
+    options: OPTIONS,
+  },
+  {
+    id: 2,
+    prompt: "I haven’t gotten enough love and attention",
+    options: OPTIONS,
+  },
+  {
+    id: 3,
+    prompt:
+      "For the most part, I haven’t had someone to depend on for advice and emotional support",
+    options: OPTIONS,
+  },
+  {
+    id: 4,
+    prompt:
+      "Most of the time, I haven’t had someone to nurture me, share him/herself with me, or care deeply about everything that happens to me.",
+    options: OPTIONS,
+  },
+  {
+    id: 5,
+    prompt:
+      "For much of my life, I haven’t had someone who wanted to get close to me and spend a lot of time with me.",
+    options: OPTIONS,
+  },
+  {
+    id: 6,
+    prompt:
+      "In general, people have not been there to give me warmth, holding and affection",
+    options: OPTIONS,
+  },
+  {
+    id: 7,
+    prompt: "For much of my life, I haven’t felt that I am special to someone",
+    options: OPTIONS,
+  },
+  {
+    id: 8,
+    prompt:
+      "For the most part, I have not had someone who really listens to me, understands me, or is tuned into my true needs and feelings",
+    options: OPTIONS,
+  },
+  {
+    id: 9,
+    prompt:
+      "I have rarely had a strong person to give me sound advice or direction when I’m not sure what to do.",
+    options: OPTIONS,
+  },
+];
+
+export default YSQEmotionalDeprivation;
