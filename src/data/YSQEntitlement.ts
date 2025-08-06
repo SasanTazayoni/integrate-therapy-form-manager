40d145d5018b307cb825a7906ea4b6314b656d43
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:30:35 +0100

    Add Entitlement questions

---
+++ src/data/YSQEntitlement.ts
@@ -0,0 +1,67 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQEntitlement: Item[] = [
+  {
+    id: 168,
+    prompt:
+      "I have a lot of trouble accepting “no” for an answer when I want something from other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 169,
+    prompt: "I often get angry or irritable if I can’t get what I want.",
+    options: OPTIONS,
+  },
+  {
+    id: 170,
+    prompt:
+      "I’m special and shouldn’t have to accept many of the restrictions placed on other people",
+    options: OPTIONS,
+  },
+  {
+    id: 171,
+    prompt: "I hate to be constrained or kept from doing what I want",
+    options: OPTIONS,
+  },
+  {
+    id: 172,
+    prompt:
+      "I feel that I shouldn’t have to follow the normal rules and conventions other people do",
+    options: OPTIONS,
+  },
+  {
+    id: 173,
+    prompt:
+      "I feel that what I have to offer is of greater value than the contributions of others",
+    options: OPTIONS,
+  },
+  {
+    id: 174,
+    prompt: "I usually put my needs ahead of the needs of others",
+    options: OPTIONS,
+  },
+  {
+    id: 175,
+    prompt:
+      "I often find that I am so involved in my own priorities that I don’t have time to give to friends or family",
+    options: OPTIONS,
+  },
+  {
+    id: 176,
+    prompt:
+      "People often tell me I am very controlling about the ways things are done",
+    options: OPTIONS,
+  },
+  {
+    id: 177,
+    prompt: "I get very irritated when people won’t do what I ask of them",
+    options: OPTIONS,
+  },
+  {
+    id: 178,
+    prompt: "I can’t tolerate other people telling me what to do.",
+    options: OPTIONS,
+  },
+];
+
+export default YSQEntitlement;
