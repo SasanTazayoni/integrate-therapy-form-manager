d2663b2bfcd223fc4a8dd66d62b54f7742b62b61
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:27:10 +0100

    Add Failure questions

---
+++ src/data/YSQFailure.ts
@@ -0,0 +1,58 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQFailure: Item[] = [
+  {
+    id: 69,
+    prompt:
+      "Almost nothing I do at work (or school) is as good as other people can do",
+    options: OPTIONS,
+  },
+  {
+    id: 70,
+    prompt: "I’m incompetent when it comes to achievement",
+    options: OPTIONS,
+  },
+  {
+    id: 71,
+    prompt:
+      "Most other people are more capable than I am in areas of work (or school) and achievement",
+    options: OPTIONS,
+  },
+  {
+    id: 72,
+    prompt: "I’m a failure",
+    options: OPTIONS,
+  },
+  {
+    id: 73,
+    prompt:
+      "I’m not as talented as most people are at their work (or at school)",
+    options: OPTIONS,
+  },
+  {
+    id: 74,
+    prompt:
+      "I’m not as intelligent as most people when it comes to work (or school)",
+    options: OPTIONS,
+  },
+  {
+    id: 75,
+    prompt:
+      "I am humiliated by my failures and inadequacies in the work (or school) sphere",
+    options: OPTIONS,
+  },
+  {
+    id: 76,
+    prompt:
+      "I often feel embarrassed around other people, because I don’t measure up to them in terms of my accomplishments",
+    options: OPTIONS,
+  },
+  {
+    id: 77,
+    prompt:
+      "I often compare my accomplishments with others and feel that they are much more successful",
+    options: OPTIONS,
+  },
+];
+
+export default YSQFailure;
