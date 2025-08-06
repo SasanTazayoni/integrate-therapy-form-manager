2c3cdae3f116a4d1812667887089c4262c914207
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-06 20:25:57 +0100

    Add Social Isolation questions

---
+++ src/data/YSQSocialIsolation.ts
@@ -0,0 +1,56 @@
+import { Item, OPTIONS } from "./YSQCommon";
+
+const YSQSocialIsolation: Item[] = [
+  {
+    id: 44,
+    prompt: "I don’t fit in.",
+    options: OPTIONS,
+  },
+  {
+    id: 45,
+    prompt: "I’m fundamentally different from other people",
+    options: OPTIONS,
+  },
+  {
+    id: 46,
+    prompt: "I don’t belong; I’m a loner",
+    options: OPTIONS,
+  },
+  {
+    id: 47,
+    prompt: "I feel alienated from other people",
+    options: OPTIONS,
+  },
+  {
+    id: 48,
+    prompt: "I feel isolated and alone",
+    options: OPTIONS,
+  },
+  {
+    id: 49,
+    prompt: "I always feel on the outside of groups",
+    options: OPTIONS,
+  },
+  {
+    id: 50,
+    prompt: "No one really understands me",
+    options: OPTIONS,
+  },
+  {
+    id: 51,
+    prompt: "My family was always different from the families around us",
+    options: OPTIONS,
+  },
+  {
+    id: 52,
+    prompt: "I sometimes feel as if I’m an alien",
+    options: OPTIONS,
+  },
+  {
+    id: 53,
+    prompt: "If I disappeared tomorrow, no one would notice",
+    options: OPTIONS,
+  },
+];
+
+export default YSQSocialIsolation;
