32a30b13a5790d234da5d7058610089812674e63
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:34:47 +0100

    Add YSQNegativityPessimism item set with NP category

--- src/data/YSQNegativityPessimism.ts
+++ src/data/YSQNegativityPessimism.ts
@@ -6,62 +6,73 @@ const YSQNegativityPessimism: Item[] = [
     prompt:
       "Even when things seem to be going well, I feel that it is only temporary",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 209,
     prompt:
       "If something good happens, I worry that something bad is likely to follow",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 210,
     prompt: "You can't be too careful; something will almost always go wrong",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 211,
     prompt:
       "No matter how hard I work, I worry that I could be wiped out financially",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 212,
     prompt: "I worry that a wrong decision could lead to disaster.",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 213,
     prompt:
       "I often obsess over minor decisions, because the consequences of making a mistake seem so serious",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 214,
     prompt:
       "I feel better assuming that things will not work out for me, so that I don’t feel disappointed if things go wrong",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 215,
     prompt:
       "I focus more on the negative aspects of life and of events than on the positive",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 216,
     prompt: "I tend to be pessimistic",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 217,
     prompt: "People close to me consider me a worrier",
     options: OPTIONS,
+    category: "NP",
   },
   {
     id: 218,
     prompt:
       "If people get too enthusiastic about something, I become uncomfortable and feel like warning them of what could go wrong",
     options: OPTIONS,
+    category: "NP",
   },
 ];
 
