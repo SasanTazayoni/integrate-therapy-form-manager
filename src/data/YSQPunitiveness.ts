6be8b6ec4db64bbec158619f1d8ab3a977076ae0
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:09:21 +0100

    Add YSQPunitiveness item set with PU category

--- src/data/YSQPunitiveness.ts
+++ src/data/YSQPunitiveness.ts
@@ -5,77 +5,91 @@ const YSQPunitiveness: Item[] = [
     id: 219,
     prompt: "If I make a mistake, I deserve to be punished",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 220,
     prompt: "If I don’t try my hardest, I should expect to lose out",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 221,
     prompt: "There is no excuse if I make a mistake",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 222,
     prompt:
       "People who don’t “pull their own weight” should get punished in some way",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 223,
     prompt:
       "Most of the time, I don’t accept the excuses other people make. They’re just not willing to accept responsibility and pay the consequences",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 224,
     prompt: "If I don’t do the job, I should suffer the consequences",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 225,
     prompt: "I often think about mistakes I’ve made and feel angry with myself",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 226,
     prompt:
       "When people do something bad, I have trouble applying the phrase “Forgive and forget”",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 227,
     prompt: "I hold grudges, even after someone has apologized.",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 228,
     prompt:
       "I get upset when I think someone has been “let off the hook” too easily",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 229,
     prompt:
       "I get angry when people make excuses for themselves, or blame other people for their problems",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 230,
     prompt:
       "It doesn’t matter why I make a mistake; when I do something wrong, I should pay the price",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 231,
     prompt: "I “beat up” on myself a lot for the things I screw up",
     options: OPTIONS,
+    category: "PU",
   },
   {
     id: 232,
     prompt: "I’m a bad person who deserves to be punished.",
     options: OPTIONS,
+    category: "PU",
   },
 ];
 
