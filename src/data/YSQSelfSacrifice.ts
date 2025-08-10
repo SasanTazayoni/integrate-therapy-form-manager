e3c55fda099884f08eea2dcfe005a38e20daf5c3
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:36:34 +0100

    Add YSQSelfSacrifice item set with SS category

--- src/data/YSQSelfSacrifice.ts
+++ src/data/YSQSelfSacrifice.ts
@@ -5,90 +5,107 @@ const YSQSelfSacrifice: Item[] = [
     id: 126,
     prompt: "I put others’ needs before my own, or else I feel guilty",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 127,
     prompt: "I feel guilty when I let other people down or disappoint them",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 128,
     prompt: "I give more to other people than I get back in return",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 129,
     prompt:
       "I’m the one who usually ends up taking care of the people I’m close to",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 130,
     prompt: "There is almost nothing I couldn’t put up with if I loved someone",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 131,
     prompt: "I am a good person because I think of others more than of myself",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 132,
     prompt:
       "At work, I’m usually the one to volunteer to do extra tasks or to put in extra time",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 133,
     prompt: "No matter how busy I am, I can always find time for others",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 134,
     prompt: "I can get by on very little, because my needs are minimal",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 135,
     prompt: "I’m only happy when those around me are happy",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 136,
     prompt:
       "I’m so busy doing for the people that I care about that I have little time for myself",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 137,
     prompt: "I’ve always been the one who listens to everyone else’s problems",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 138,
     prompt: "I’m more comfortable giving a present than receiving one",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 139,
     prompt:
       "Other people see me as doing too much for others and not enough for myself",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 140,
     prompt: "No matter how much I give, I feel it is never enough",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 141,
     prompt: "If I do what I want, I feel very uncomfortable",
     options: OPTIONS,
+    category: "SS",
   },
   {
     id: 142,
     prompt: "It’s very difficult for me to ask others to take care of my needs",
     options: OPTIONS,
+    category: "SS",
   },
 ];
 
