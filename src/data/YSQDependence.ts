09fac260c501c753252bc9735077cf88b3e72380
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:32:47 +0100

    Add YSQDependence item set with DI category

--- src/data/YSQDependence.ts
+++ src/data/YSQDependence.ts
@@ -5,83 +5,98 @@ const YSQDependence: Item[] = [
     id: 78,
     prompt: "I do not feel capable of getting by on my own in everyday life",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 79,
     prompt: "I need other people to help me get by",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 80,
     prompt: "I do not feel I can cope well by myself",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 81,
     prompt:
       "I believe that other people can take care of me better than I can take care of myself",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 82,
     prompt:
       "I have trouble tackling new tasks outside of work, unless I have someone to guide me",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 83,
     prompt:
       "I think of myself as a dependent person when it comes to everyday functioning",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 84,
     prompt: "I screw up everything I try, even outside of work (or school)",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 85,
     prompt: "I’m inept in most areas of life",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 86,
     prompt:
       "If I trust my own judgement in everyday situations, I’ll make the wrong decision",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 87,
     prompt: "I lack common sense",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 88,
     prompt: "My judgement cannot be relied on in everyday situations",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 89,
     prompt:
       "I don’t feel confident about my ability to solve everyday problems that come up",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 90,
     prompt:
       "I feel that I need someone I can rely on to give me advice about practical issues",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 91,
     prompt:
       "I feel more like a child than an adult when it comes to handling everyday responsibilities",
     options: OPTIONS,
+    category: "DI",
   },
   {
     id: 92,
     prompt: "I find the responsibilities of everyday life overwhelming",
     options: OPTIONS,
+    category: "DI",
   },
 ];
 
