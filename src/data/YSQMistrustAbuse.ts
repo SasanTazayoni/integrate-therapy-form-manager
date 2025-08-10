8925ad092b5ef4a245a8f1af7a6f4d0f46c23890
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:34:11 +0100

    Add YSQMistrustAbuse item set with MA category

--- src/data/YSQMistrustAbuse.ts
+++ src/data/YSQMistrustAbuse.ts
@@ -5,95 +5,112 @@ const YSQMistrustAbuse: Item[] = [
     id: 27,
     prompt: "I feel that people will take advantage of me",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 28,
     prompt: "I often feel that I have to protect myself from other people",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 29,
     prompt:
       "I feel that I cannot let my guard down in the presence of other people, or else they will intentionally hurt me",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 30,
     prompt:
       "If someone acts nicely towards me, I assume that he/she must be after something",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 31,
     prompt: "It is only a matter of time before someone betrays me",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 32,
     prompt: "Most people only think about themselves",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 33,
     prompt: "I have a great deal of difficulty trusting people",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 34,
     prompt: "I am quite suspicious of other people’s motives",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 35,
     prompt:
       "Other people are rarely honest, they are usually not what they appear",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 36,
     prompt: "I’m usually on the lookout for people’s ulterior motives",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 37,
     prompt:
       "If I think someone is out to hurt me, I try to hurt him or her first",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 38,
     prompt:
       "People usually have to prove themselves to me before I can trust them",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 39,
     prompt:
       "I set up ‘tests’ for other people, to see if they are telling me the truth and are well-intentioned",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 40,
     prompt: "I subscribe to the belief: ‘Control or be controlled.’",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 41,
     prompt:
       "I get angry when I think about the ways I have been mistreated by other people during my life",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 42,
     prompt:
       "During my life, those close to me have taken advantage of me or used me for their own purpose",
     options: OPTIONS,
+    category: "MA",
   },
   {
     id: 43,
     prompt:
       "I have been physically, emotionally, or sexually abused by important people in my life",
     options: OPTIONS,
+    category: "MA",
   },
 ];
 
