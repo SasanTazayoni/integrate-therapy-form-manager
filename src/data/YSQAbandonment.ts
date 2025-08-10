c09f860555d90b3a06f4a5952a81e6e2401d9029
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:37:36 +0100

    Add YSQAbandonment item set with AB category

--- src/data/YSQAbandonment.ts
+++ src/data/YSQAbandonment.ts
@@ -6,98 +6,115 @@ const YSQAbandonment: Item[] = [
     prompt:
       "I worry that the people I love will die soon, even though there is little medical reason to support my concern",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 11,
     prompt:
       "I find myself clinging to people I’m close to, because I’m afraid they’ll leave me",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 12,
     prompt: "I worry that people I feel close to will leave me or abandon me",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 13,
     prompt: "I feel that I lack a stable base of emotional support",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 14,
     prompt:
       "I don’t feel that important relationships will last; I expect them to end",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 15,
     prompt:
       "I feel addicted to partners who can’t be there for me in a committed way",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 16,
     prompt: "In the end, I will be alone",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 17,
     prompt:
       "When I feel someone I care for pulling away from me, I get desperate",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 18,
     prompt:
       "Sometimes I am so worried about people leaving me that I drive them away",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 19,
     prompt:
       "I become upset when someone leaves me alone, even for a short period of time",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 20,
     prompt:
       "I can’t count on people who support me to be there on a regular basis",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 21,
     prompt:
       "I can’t let myself get really close to other people, because I can’t be sure they’ll always be there",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 22,
     prompt:
       "It seems that the important people in my life are always coming and going",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 23,
     prompt:
       "I worry a lot that the people I love will find someone else they prefer and leave me",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 24,
     prompt:
       "The people close to me have been very unpredictable; one moment they’re available and nice to me; the next they’re angry, upset, self-absorbed, fighting, etc.",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 25,
     prompt: "I need other people so much that I worry about losing them",
     options: OPTIONS,
+    category: "AB",
   },
   {
     id: 26,
     prompt:
       "I can’t be myself or express what I really feel, or people will leave me",
     options: OPTIONS,
+    category: "AB",
   },
 ];
 
