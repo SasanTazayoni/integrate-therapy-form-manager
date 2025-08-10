71d3bb5325aad0e01931171f39d51a3246483825
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:37:08 +0100

    Add YSQUnrelentingStandards item set with US category

--- src/data/YSQUnrelentingStandards.ts
+++ src/data/YSQUnrelentingStandards.ts
@@ -6,85 +6,101 @@ const YSQUnrelentingStandards: Item[] = [
     prompt:
       "I must be the best at most of what I do: I can’t accept second best",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 153,
     prompt: "I strive to keep almost everything in perfect order",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 154,
     prompt: "I must look my best most of the time",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 155,
     prompt: "I try to do my best: I can’t settle for ‘good enough’",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 156,
     prompt:
       "I have so much to accomplish that there is almost no time to really relax",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 157,
     prompt: "Almost nothing I do is quite good enough, I can always do better",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 158,
     prompt: "I must meet all my responsibilities",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 159,
     prompt:
       "I feel there is constant pressure for me to achieve and get things done",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 160,
     prompt: "My relationships suffer because I push myself so hard",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 161,
     prompt:
       "My health is suffering because I put myself under so much pressure to do well",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 162,
     prompt: "I often sacrifice pleasure and happiness to meet my own standards",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 163,
     prompt: "When I make a mistake, I deserve strong criticism",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 164,
     prompt:
       "I can’t let myself off the hook easily or make excuses for my mistakes",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 165,
     prompt: "I’m a very competitive person",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 166,
     prompt: "I put a good deal of emphasis on money or status",
     options: OPTIONS,
+    category: "US",
   },
   {
     id: 167,
     prompt: "I always have to be Number One, in terms of my performance.",
     options: OPTIONS,
+    category: "US",
   },
 ];
 
