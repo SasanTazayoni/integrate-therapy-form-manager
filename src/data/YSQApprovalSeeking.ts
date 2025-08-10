d09d479f2dcdc68ef7b07186877495041c46e381
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:07:39 +0100

    Add YSQApprovalSeeking item set with AS category

--- src/data/YSQApprovalSeeking.ts
+++ src/data/YSQApprovalSeeking.ts
@@ -5,80 +5,94 @@ const YSQApprovalSeeking: Item[] = [
     id: 194,
     prompt: "It is important to me to be liked by almost everyone I know.",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 195,
     prompt:
       "I change myself depending on the people I’m with, so they’ll like me more",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 196,
     prompt: "I try hard to fit in",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 197,
     prompt: "My self-esteem is based mostly on how other people view me",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 198,
     prompt: "Having money and knowing important people make me feel worthwhile",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 199,
     prompt:
       "I spend a lot of time on my physical appearance so people will value me",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 200,
     prompt:
       "Accomplishments are most valuable to me if other people notice them",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 201,
     prompt:
       "I am so focused on fitting in that sometimes I don’t know who I am",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 202,
     prompt:
       "I find it hard to set my own goals, without taking into account how others will respond to my choices",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 203,
     prompt:
       "When I look at my life decisions, I see that I made most of them with other people’s approval in mind",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 204,
     prompt: "Even if I don’t like someone, I still want him or her to like me",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 205,
     prompt:
       "Unless I get a lot of attention from others, I feel less important",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 206,
     prompt:
       "If I make remarks at a meeting or am introduced at a gathering, I look forward to recognition and admiration",
     options: OPTIONS,
+    category: "AS",
   },
   {
     id: 207,
     prompt:
       "Lots of praise and compliments make me feel like a worthwhile person",
     options: OPTIONS,
+    category: "AS",
   },
 ];
 
