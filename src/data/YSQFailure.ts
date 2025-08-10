67f18d3b5281fc9451291c7764fc32c1c1752e46
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:10:29 +0100

    Add YSQFailure item set with FA category

--- src/data/YSQFailure.ts
+++ src/data/YSQFailure.ts
@@ -6,52 +6,61 @@ const YSQFailure: Item[] = [
     prompt:
       "Almost nothing I do at work (or school) is as good as other people can do",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 70,
     prompt: "I’m incompetent when it comes to achievement",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 71,
     prompt:
       "Most other people are more capable than I am in areas of work (or school) and achievement",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 72,
     prompt: "I’m a failure",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 73,
     prompt:
       "I’m not as talented as most people are at their work (or at school)",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 74,
     prompt:
       "I’m not as intelligent as most people when it comes to work (or school)",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 75,
     prompt:
       "I am humiliated by my failures and inadequacies in the work (or school) sphere",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 76,
     prompt:
       "I often feel embarrassed around other people, because I don’t measure up to them in terms of my accomplishments",
     options: OPTIONS,
+    category: "FA",
   },
   {
     id: 77,
     prompt:
       "I often compare my accomplishments with others and feel that they are much more successful",
     options: OPTIONS,
+    category: "FA",
   },
 ];
 
