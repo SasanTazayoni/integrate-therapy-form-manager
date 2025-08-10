d41002ef71dd837bdf76117b93bb06aa6f725f97
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:33:36 +0100

    Add YSQSubjugation item set with SB category

--- src/data/YSQSubjugation.ts
+++ src/data/YSQSubjugation.ts
@@ -6,57 +6,67 @@ const YSQSubjugation: Item[] = [
     prompt:
       "I let other people have their way, because I fear the consequences",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 117,
     prompt: "I believe that if I do what I want, I’m only asking for trouble",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 118,
     prompt:
       "I feel that I have no choice but to give in to other people’s wishes, or else they will retaliate or reject me in some way",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 119,
     prompt: "In relationships, I let the other person have the upper hand",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 120,
     prompt:
       "I’ve always let others make choices for me, so I really don’t know what I want for myself",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 121,
     prompt: "I feel the major decisions in my life were not really my own",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 122,
     prompt:
       "I worry a lot about pleasing other people, so they won’t reject me",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 123,
     prompt:
       "I have a lot of trouble demanding that my rights be respected and that my feelings be taken into account",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 124,
     prompt:
       "I get back at people in little ways instead of showing my anger directly",
     options: OPTIONS,
+    category: "SB",
   },
   {
     id: 125,
     prompt:
       "I will go to much greater lengths than most people to avoid confrontations",
     options: OPTIONS,
+    category: "SB",
   },
 ];
 
