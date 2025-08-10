b0d15698e3b116aff3fe4ad4fb9c554b71165c8b
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:05:37 +0100

    Add YSQEnmeshment item set with EU category

--- src/data/YSQEnmeshment.ts
+++ src/data/YSQEnmeshment.ts
@@ -6,66 +6,77 @@ const YSQEnmeshment: Item[] = [
     prompt:
       "I have not been able to separate myself from my parent(s), the way other people my age seem to",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 106,
     prompt:
       "My parent(s) and I tend to be over involved in each other’s lives and problems",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 107,
     prompt:
       "It is very difficult for my parent(s) and me to keep intimate details from each other, without feeling betrayed or guilty",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 108,
     prompt:
       "My parent(s) and I have to speak to each other almost every day, or else one of us feels guilty, hurt, disappointed or alone",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 109,
     prompt:
       "I often feel that I don’t have a separate identity from my parents or partner",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 110,
     prompt:
       "I often feel as if my parent(s) are living through me – I don’t have a life of my own",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 111,
     prompt:
       "It is very difficult for me to maintain any distance from the people I am intimate with, I have trouble keeping any separate sense of myself",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 112,
     prompt:
       "I am so involved with my partner or parent(s) that I do not really know who I am or what I want",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 113,
     prompt:
       "I have trouble separating my point of view or opinion from that of my parent(s) or partner",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 114,
     prompt:
       "I often feel that I have no privacy when it comes to my parent(s) or partner",
     options: OPTIONS,
+    category: "EU",
   },
   {
     id: 115,
     prompt:
       "I feel that my parent(s) are, or would be, very hurt about my living on my own, away from them",
     options: OPTIONS,
+    category: "EU",
   },
 ];
 
