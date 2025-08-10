5cd73be23280468217117f825b06d094a6a75668
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:07:04 +0100

    Add YSQEmotionalDeprivation item set with ED category

--- src/data/YSQEmotionalDeprivation.ts
+++ src/data/YSQEmotionalDeprivation.ts
@@ -5,52 +5,61 @@ const YSQEmotionalDeprivation: Item[] = [
     id: 1,
     prompt: "People have not been there to meet my emotional needs",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 2,
     prompt: "I haven’t gotten enough love and attention",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 3,
     prompt:
       "For the most part, I haven’t had someone to depend on for advice and emotional support",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 4,
     prompt:
       "Most of the time, I haven’t had someone to nurture me, share him/herself with me, or care deeply about everything that happens to me.",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 5,
     prompt:
       "For much of my life, I haven’t had someone who wanted to get close to me and spend a lot of time with me.",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 6,
     prompt:
       "In general, people have not been there to give me warmth, holding and affection",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 7,
     prompt: "For much of my life, I haven’t felt that I am special to someone",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 8,
     prompt:
       "For the most part, I have not had someone who really listens to me, understands me, or is tuned into my true needs and feelings",
     options: OPTIONS,
+    category: "ED",
   },
   {
     id: 9,
     prompt:
       "I have rarely had a strong person to give me sound advice or direction when I’m not sure what to do.",
     options: OPTIONS,
+    category: "ED",
   },
 ];
 
