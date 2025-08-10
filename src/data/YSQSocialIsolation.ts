8fb2f9892567b8907de0b8b6392ecc263eef42f1
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:11:16 +0100

    Add YSQSocialIsolation item set with SI category

--- src/data/YSQSocialIsolation.ts
+++ src/data/YSQSocialIsolation.ts
@@ -5,51 +5,61 @@ const YSQSocialIsolation: Item[] = [
     id: 44,
     prompt: "I don’t fit in.",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 45,
     prompt: "I’m fundamentally different from other people",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 46,
     prompt: "I don’t belong; I’m a loner",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 47,
     prompt: "I feel alienated from other people",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 48,
     prompt: "I feel isolated and alone",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 49,
     prompt: "I always feel on the outside of groups",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 50,
     prompt: "No one really understands me",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 51,
     prompt: "My family was always different from the families around us",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 52,
     prompt: "I sometimes feel as if I’m an alien",
     options: OPTIONS,
+    category: "SI",
   },
   {
     id: 53,
     prompt: "If I disappeared tomorrow, no one would notice",
     options: OPTIONS,
+    category: "SI",
   },
 ];
 
