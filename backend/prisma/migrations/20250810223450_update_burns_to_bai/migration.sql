97293b6496b512fb570e2197eda877f13f90b153
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 23:37:27 +0100

    Add migration to rename burns_score to bai_score

---
+++ backend/prisma/migrations/20250810223450_update_burns_to_bai/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `burns_score` on the `Form` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Form" DROP COLUMN "burns_score",
+ADD COLUMN     "bai_score" TEXT;
