e8add65b68a0f4eed6e5d414ac3cbbd54818159b
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:39:23 +0100

    Add YSQEmotionalInhibition item set with EI category

--- src/data/YSQEmotionalInhibition.ts
+++ src/data/YSQEmotionalInhibition.ts
@@ -5,50 +5,59 @@ const YSQEmotionalInhibition: Item[] = [
     id: 143,
     prompt: "I worry about losing control of my actions",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 144,
     prompt:
       "I worry that I might seriously harm someone physically or emotionally, if my anger gets out of control",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 145,
     prompt:
       "I feel that I must control my emotions and impulses, or something bad is likely to happen",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 146,
     prompt:
       "A lot of anger and resentment build up inside of me that I don’t express",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 147,
     prompt:
       "I am too self-conscious to show positive feelings to others (e.g. affection, showing I care)",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 148,
     prompt: "I find it embarrassing to express my feelings to others",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 149,
     prompt: "I find it hard to be warm and spontaneous",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 150,
     prompt: "I control myself so much that people think I am unemotional",
     options: OPTIONS,
+    category: "EI",
   },
   {
     id: 151,
     prompt: "People see me as uptight emotionally",
     options: OPTIONS,
+    category: "EI",
   },
 ];
 
