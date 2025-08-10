f2653880811995f2999a8a46ccdcccba2e306a78
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-10 12:38:19 +0100

    Add YSQInsufficientSelfControl item set with IS category

--- src/data/YSQInsufficientSelfControl.ts
+++ src/data/YSQInsufficientSelfControl.ts
@@ -6,83 +6,98 @@ const YSQInsufficientSelfControl: Item[] = [
     prompt:
       "I have great difficulty getting myself to stop drinking, smoking, overeating, or other problem behaviours.",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 180,
     prompt:
       "I can’t seem to discipline myself to complete routine or boring tasks",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 181,
     prompt:
       "Often I allow myself to carry through on impulses and express emotions that get me into trouble or hurt other people",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 182,
     prompt: "If I can’t reach a goal, I become easily frustrated and give up",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 183,
     prompt:
       "I have a very difficult time sacrificing immediate gratification to achieve a long-range goal",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 184,
     prompt:
       "It often happens that, once I start to feel angry, I just can’t control it",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 185,
     prompt: "I tend to overdo things, even though I know they are bad for me",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 186,
     prompt: "I get bored very easily",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 187,
     prompt:
       "When tasks become difficult, I usually cannot persevere and complete them",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 188,
     prompt: "I can’t concentrate on anything for too long",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 189,
     prompt:
       "I can’t force myself to do things I don’t enjoy, even when I know it’s for my own good",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 190,
     prompt: "I lose my temper at the slightest offence",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 191,
     prompt: "I have rarely been able to stick to resolutions",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 192,
     prompt:
       "I can almost never hold back from showing people how I really feel, no matter what the cost may be",
     options: OPTIONS,
+    category: "IS",
   },
   {
     id: 193,
     prompt: "I often do things impulsively that I later regret",
     options: OPTIONS,
+    category: "IS",
   },
 ];
 
