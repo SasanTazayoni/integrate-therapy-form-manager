885bcdb5dc22ed827f96055d6ff23bfa0c54ac31
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-11 19:58:31 +0100

    Add full set of 124 SMI questionnaire items

---
+++ src/data/SMIItems.ts
@@ -0,0 +1,608 @@
+import { Item, OPTIONS } from "./SMICommon";
+
+const SMIItems: Item[] = [
+  {
+    id: 1,
+    prompt:
+      "By showing others that you are not to be trifled with, you command respect.",
+    options: OPTIONS,
+  },
+  { id: 2, prompt: "I feel loved and accepted.", options: OPTIONS },
+  {
+    id: 3,
+    prompt: "I deny myself pleasure because I don’t deserve it.",
+    options: OPTIONS,
+  },
+  {
+    id: 4,
+    prompt: "I feel fundamentally inadequate, flawed, or defective.",
+    options: OPTIONS,
+  },
+  {
+    id: 5,
+    prompt:
+      "I have an impulse to punish myself by hurting myself (e.g., cutting myself).",
+    options: OPTIONS,
+  },
+  { id: 6, prompt: "I feel lost.", options: OPTIONS },
+  { id: 7, prompt: "I’m hard on myself.", options: OPTIONS },
+  {
+    id: 8,
+    prompt:
+      "I try very hard to please other people in order to avoid conflict, confrontation, or rejection.",
+    options: OPTIONS,
+  },
+  { id: 9, prompt: "I can’t forgive myself.", options: OPTIONS },
+  {
+    id: 10,
+    prompt: "I do things to make myself the center of attention.",
+    options: OPTIONS,
+  },
+  {
+    id: 11,
+    prompt: "I get irritated when people don’t do what I ask them to do.",
+    options: OPTIONS,
+  },
+  {
+    id: 12,
+    prompt: "I have trouble controlling my impulses.",
+    options: OPTIONS,
+  },
+  {
+    id: 13,
+    prompt: "If I can’t reach a goal, I become easily frustrated and give up.",
+    options: OPTIONS,
+  },
+  { id: 14, prompt: "I have violent outbursts.", options: OPTIONS },
+  {
+    id: 15,
+    prompt:
+      "I act impulsively or express emotions that get me into trouble or hurt other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 16,
+    prompt: "It’s my fault when something bad happens.",
+    options: OPTIONS,
+  },
+  { id: 17, prompt: "I feel content and at ease.", options: OPTIONS },
+  {
+    id: 18,
+    prompt:
+      "I change myself depending on the people I’m with, so they’ll like me or approve of me.",
+    options: OPTIONS,
+  },
+  { id: 19, prompt: "I feel connected to other people.", options: OPTIONS },
+  {
+    id: 20,
+    prompt: "When there are problems, I try hard to solve them myself.",
+    options: OPTIONS,
+  },
+  {
+    id: 21,
+    prompt: "I don’t discipline myself to complete routine or boring tasks.",
+    options: OPTIONS,
+  },
+  {
+    id: 22,
+    prompt: "If I don’t fight, I will be abused or neglected.",
+    options: OPTIONS,
+  },
+  {
+    id: 23,
+    prompt: "I have to take care of the people around me.",
+    options: OPTIONS,
+  },
+  {
+    id: 24,
+    prompt: "Who allows him/herself to be made fun of, is a loser.",
+    options: OPTIONS,
+  },
+  {
+    id: 25,
+    prompt: "I physically attack others when I am angry at them.",
+    options: OPTIONS,
+  },
+  {
+    id: 26,
+    prompt:
+      "Once I start to feel angry, I often don’t control it and lose my temper.",
+    options: OPTIONS,
+  },
+  {
+    id: 27,
+    prompt:
+      "It’s important for me to be Number One (e.g., the most popular, most successful, most wealthy, most powerful).",
+    options: OPTIONS,
+  },
+  { id: 28, prompt: "I feel indifferent.", options: OPTIONS },
+  {
+    id: 29,
+    prompt:
+      "I can solve problems rationally without letting my emotions overwhelm me.",
+    options: OPTIONS,
+  },
+  {
+    id: 30,
+    prompt: "I find it nonsense to make a plan how to handle something.",
+    options: OPTIONS,
+  },
+  {
+    id: 31,
+    prompt: "I will not settle for the second best.",
+    options: OPTIONS,
+  },
+  { id: 32, prompt: "Attack is the best defence.", options: OPTIONS },
+  { id: 33, prompt: "I feel cold towards other people.", options: OPTIONS },
+  {
+    id: 34,
+    prompt:
+      "I feel detached (no contact with myself, my emotions or other people).",
+    options: OPTIONS,
+  },
+  { id: 35, prompt: "I follow my emotions blindly.", options: OPTIONS },
+  { id: 36, prompt: "I feel desperate.", options: OPTIONS },
+  {
+    id: 37,
+    prompt: "I allow other people to criticize me or put me down.",
+    options: OPTIONS,
+  },
+  {
+    id: 38,
+    prompt: "In relationships, I let the other person have the upper hand.",
+    options: OPTIONS,
+  },
+  { id: 39, prompt: "I feel distant from other people.", options: OPTIONS },
+  {
+    id: 40,
+    prompt:
+      "I act impulsively or express emotions that get me into trouble or hurt other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 41,
+    prompt:
+      "I work or sport intensively so I do not have to think about annoying feelings.",
+    options: OPTIONS,
+  },
+  {
+    id: 42,
+    prompt:
+      "I ‘m angry that people are trying to take away my freedom or independence.",
+    options: OPTIONS,
+  },
+  {
+    id: 43,
+    prompt: "I feel nothing.",
+    options: OPTIONS,
+  },
+  {
+    id: 44,
+    prompt:
+      "I do what I want to do, regardless of other people’s needs and feelings.",
+    options: OPTIONS,
+  },
+  {
+    id: 45,
+    prompt:
+      "I’m not letting myself relax or have fun until I’ve finished everything I’m supposed to do.",
+    options: OPTIONS,
+  },
+  {
+    id: 46,
+    prompt: "I throw things around when I am angry.",
+    options: OPTIONS,
+  },
+  {
+    id: 47,
+    prompt: "I feel enraged at someone.",
+    options: OPTIONS,
+  },
+  {
+    id: 48,
+    prompt: "I feel that I fit in with other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 49,
+    prompt:
+      "I have a lot of anger built up inside of me that I need to let out.",
+    options: OPTIONS,
+  },
+  {
+    id: 50,
+    prompt: "I feel lonely.",
+    options: OPTIONS,
+  },
+  {
+    id: 51,
+    prompt: "I try to do my best at everything I do.",
+    options: OPTIONS,
+  },
+  {
+    id: 52,
+    prompt:
+      "I like doing something exciting or soothing to avoid my feelings (e.g., working, gambling, eating, shopping, sexual activities, watching TV).",
+    options: OPTIONS,
+  },
+  {
+    id: 53,
+    prompt: "Equality does not exist, so you best stand above others.",
+    options: OPTIONS,
+  },
+  {
+    id: 54,
+    prompt: "In my anger, I loose control over myself and I threaten others.",
+    options: OPTIONS,
+  },
+  {
+    id: 55,
+    prompt:
+      "I let other people get their own way instead of expressing my own needs.",
+    options: OPTIONS,
+  },
+  {
+    id: 56,
+    prompt: "If someone is not with me they are against me.",
+    options: OPTIONS,
+  },
+  {
+    id: 57,
+    prompt:
+      "In order to be bothered less from my annoying thoughts or feelings, I make sure that I am always busy.",
+    options: OPTIONS,
+  },
+  {
+    id: 58,
+    prompt: "I’m bad if I get angry at other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 59,
+    prompt: "I don’t want to get involved with people.",
+    options: OPTIONS,
+  },
+  {
+    id: 60,
+    prompt:
+      "I have been so angry that I have (seriously) hurt someone or killed someone.",
+    options: OPTIONS,
+  },
+  {
+    id: 61,
+    prompt: "I feel that I have plenty of stability and security in my life.",
+    options: OPTIONS,
+  },
+  {
+    id: 62,
+    prompt: "I know when to express my emotions and when not to.",
+    options: OPTIONS,
+  },
+  {
+    id: 63,
+    prompt: "I’m angry with someone for leaving me alone or abandoning me.",
+    options: OPTIONS,
+  },
+  {
+    id: 64,
+    prompt: "I don’t feel connected to other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 65,
+    prompt:
+      "I can not bring myself to do things that I find boring, even if I know it is for my own good.",
+    options: OPTIONS,
+  },
+  {
+    id: 66,
+    prompt: "I break rules and regret that later.",
+    options: OPTIONS,
+  },
+  {
+    id: 67,
+    prompt: "I feel humiliated.",
+    options: OPTIONS,
+  },
+  {
+    id: 68,
+    prompt: "I trust most other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 69,
+    prompt: "I do, and think afterwards.",
+    options: OPTIONS,
+  },
+  {
+    id: 70,
+    prompt: "I get bored easily and lose interest in things.",
+    options: OPTIONS,
+  },
+  {
+    id: 71,
+    prompt: "Even if there are people around me, I feel lonely.",
+    options: OPTIONS,
+  },
+  {
+    id: 72,
+    prompt:
+      "I don’t allow myself to do pleasurable things that other people do because I’m bad.",
+    options: OPTIONS,
+  },
+  {
+    id: 73,
+    prompt: "I assert what I need without going overboard.",
+    options: OPTIONS,
+  },
+  {
+    id: 74,
+    prompt: "I feel special and better than most other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 75,
+    prompt: "I don’t care about anything; nothing matters to me.",
+    options: OPTIONS,
+  },
+  {
+    id: 76,
+    prompt:
+      "It makes me angry when someone tells me how I should feel or behave.",
+    options: OPTIONS,
+  },
+  {
+    id: 77,
+    prompt: "If you do not rule over other people, they will rule over you.",
+    options: OPTIONS,
+  },
+  {
+    id: 78,
+    prompt:
+      "I say what I feel, or do things impulsively, without thinking of the consequences.",
+    options: OPTIONS,
+  },
+  {
+    id: 79,
+    prompt: "I feel like telling people off for the way they have treated me.",
+    options: OPTIONS,
+  },
+  {
+    id: 80,
+    prompt: "I’m capable of taking care of myself.",
+    options: OPTIONS,
+  },
+  {
+    id: 81,
+    prompt: "I’m quite critical of other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 82,
+    prompt: "I’m under constant pressure to achieve and get things done.",
+    options: OPTIONS,
+  },
+  {
+    id: 83,
+    prompt:
+      "I’m trying not to make mistakes; otherwise, I’ll get down on myself.",
+    options: OPTIONS,
+  },
+  {
+    id: 84,
+    prompt: "I deserve to be punished.",
+    options: OPTIONS,
+  },
+  {
+    id: 85,
+    prompt: "I can learn, grow and change.",
+    options: OPTIONS,
+  },
+  {
+    id: 86,
+    prompt: "I want to distract myself from upsetting thoughts and feelings.",
+    options: OPTIONS,
+  },
+  {
+    id: 87,
+    prompt: "I’m angry at myself.",
+    options: OPTIONS,
+  },
+  {
+    id: 88,
+    prompt: "I feel flat.",
+    options: OPTIONS,
+  },
+  {
+    id: 89,
+    prompt: "I have to be the best in what I do.",
+    options: OPTIONS,
+  },
+  {
+    id: 90,
+    prompt:
+      "I sacrifice pleasure, health, or happiness to meet my own standards.",
+    options: OPTIONS,
+  },
+  {
+    id: 91,
+    prompt: "I’m demanding of other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 92,
+    prompt: "If I am angry, it can get so out of hand that people get hurt.",
+    options: OPTIONS,
+  },
+  {
+    id: 93,
+    prompt: "I am untouchable.",
+    options: OPTIONS,
+  },
+  {
+    id: 94,
+    prompt: "I’m a bad person.",
+    options: OPTIONS,
+  },
+  {
+    id: 95,
+    prompt: "I feel safe.",
+    options: OPTIONS,
+  },
+  {
+    id: 96,
+    prompt: "I feel listened to, understood, and validated.",
+    options: OPTIONS,
+  },
+  {
+    id: 97,
+    prompt: "It is impossible for me to control my impulses.",
+    options: OPTIONS,
+  },
+  {
+    id: 98,
+    prompt: "I break things when I am angry.",
+    options: OPTIONS,
+  },
+  {
+    id: 99,
+    prompt: "By dominating others, nothing can happen to you.",
+    options: OPTIONS,
+  },
+  {
+    id: 100,
+    prompt:
+      "I act in a passive way, even when I don’t like the way things are.",
+    options: OPTIONS,
+  },
+  {
+    id: 101,
+    prompt: "My anger gets out of control.",
+    options: OPTIONS,
+  },
+  {
+    id: 102,
+    prompt: "I make fun of others.",
+    options: OPTIONS,
+  },
+  {
+    id: 103,
+    prompt:
+      "I feel like lashing out or hurting someone for what he/she did to me.",
+    options: OPTIONS,
+  },
+  {
+    id: 104,
+    prompt:
+      "I know that there is a ‘right’ and a ‘wrong’ way to do things; I try hard to do things the right way, or else I start criticizing myself.",
+    options: OPTIONS,
+  },
+  {
+    id: 105,
+    prompt: "I often feel alone in the world.",
+    options: OPTIONS,
+  },
+  {
+    id: 106,
+    prompt: "I feel weak and helpless.",
+    options: OPTIONS,
+  },
+  {
+    id: 107,
+    prompt: "I’m lazy.",
+    options: OPTIONS,
+  },
+  {
+    id: 108,
+    prompt:
+      "It is wise to accept everything from people that are important for me.",
+    options: OPTIONS,
+  },
+  {
+    id: 109,
+    prompt: "I’ve been cheated or treated unfairly.",
+    options: OPTIONS,
+  },
+  {
+    id: 110,
+    prompt: "If I feel the urge to do something, I just do it.",
+    options: OPTIONS,
+  },
+  {
+    id: 111,
+    prompt: "I feel left out or excluded.",
+    options: OPTIONS,
+  },
+  {
+    id: 112,
+    prompt: "I belittle others.",
+    options: OPTIONS,
+  },
+  {
+    id: 113,
+    prompt: "I feel optimistic.",
+    options: OPTIONS,
+  },
+  {
+    id: 114,
+    prompt:
+      "I feel I shouldn’t have to follow the same rules that other people do.",
+    options: OPTIONS,
+  },
+  {
+    id: 115,
+    prompt:
+      "My life right now revolves around getting things done and doing them ‘right’.",
+    options: OPTIONS,
+  },
+  {
+    id: 116,
+    prompt: "I’m pushing myself to be more responsible than most other people.",
+    options: OPTIONS,
+  },
+  {
+    id: 117,
+    prompt:
+      "I can stand up for myself when I feel unfairly criticized, abused, or taken advantage of.",
+    options: OPTIONS,
+  },
+  {
+    id: 118,
+    prompt: "I don’t deserve sympathy when something bad happens to me.",
+    options: OPTIONS,
+  },
+  {
+    id: 119,
+    prompt: "I feel that nobody loves me.",
+    options: OPTIONS,
+  },
+  {
+    id: 120,
+    prompt: "I feel that I’m basically a good person.",
+    options: OPTIONS,
+  },
+  {
+    id: 121,
+    prompt:
+      "When necessary, I complete boring and routine tasks in order to accomplish things I value.",
+    options: OPTIONS,
+  },
+  {
+    id: 122,
+    prompt: "I feel spontaneous and playful.",
+    options: OPTIONS,
+  },
+  {
+    id: 123,
+    prompt: "I can be so angry that I am capable of murdering someone.",
+    options: OPTIONS,
+  },
+  {
+    id: 124,
+    prompt:
+      "I have a good sense of who I am and what I need to make myself happy.",
+    options: OPTIONS,
+  },
+];
+
+export default SMIItems;
