674af49b1db02f180b071fdba8723b53226e6564
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-11 19:44:11 +0100

    Define Item type and default OPTIONS

---
+++ src/data/SMICommon.ts
@@ -0,0 +1,14 @@
+export type Item = {
+  id: number;
+  prompt: string;
+  options: { value: 1 | 2 | 3 | 4 | 5 | 6; text: string }[];
+};
+
+export const OPTIONS: { value: 1 | 2 | 3 | 4 | 5 | 6; text: string }[] = [
+  { value: 1, text: "Never or Almost Never" },
+  { value: 2, text: "Rarely" },
+  { value: 3, text: "Occasionally" },
+  { value: 4, text: "Frequently" },
+  { value: 5, text: "Most of the time" },
+  { value: 6, text: "All of the time" },
+];
