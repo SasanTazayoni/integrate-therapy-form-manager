fc81058deef20ea0fb67c5c6c1123c726eba2bfd
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-14 17:30:19 +0100

    Remove unused FormType and Form type definition

--- backend/controllers/clientsRepository.ts
+++ backend/controllers/clientsRepository.ts
@@ -1,5 +1,4 @@
 import prisma from "../prisma/client";
-import { FormType } from "../utils/formTypes";
 
 export type Client = {
   id: string;
@@ -12,29 +11,16 @@ export type Client = {
   updated_at: Date;
 };
 
-export type Form = {
-  id: string;
-  clientId: string;
-  form_type: FormType | string;
-  token: string;
-  token_sent_at: Date;
-  token_expires_at: Date;
-  is_active: boolean;
-  submitted_at: Date | null;
-  revoked_at: Date | null;
-  total_score?: number | null;
-  created_at: Date;
-  updated_at: Date;
-};
-
 export const findClientByEmail = async (
   email: string
 ): Promise<Client | null> => {
   return prisma.client.findUnique({ where: { email } });
 };
 
-export const getFormsByClientId = async (clientId: string): Promise<Form[]> => {
-  return prisma.form.findMany({ where: { clientId } });
+export const getFormsByClientId = async (clientId: string) => {
+  return prisma.form.findMany({
+    where: { clientId },
+  });
 };
 
 export const createNewClient = async (data: {
