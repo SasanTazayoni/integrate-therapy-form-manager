694da616662bec35b154cb0220c48b3f564ab1c5
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-15 16:01:51 +0100

    Extend ClientFormsStatus with clientName, clientDob, and smiScores

--- src/types/formStatusTypes.ts
+++ src/types/formStatusTypes.ts
@@ -2,8 +2,11 @@ import type { FormType } from "../constants/formTypes";
 
 export type ClientFormsStatus = {
   exists: boolean;
+  clientName?: string;
+  clientDob?: string;
   forms: Record<FormType, FormStatus>;
   formsCompleted?: number;
+  smiScores?: Record<string, number | null>;
 };
 
 export type FormStatus = {
