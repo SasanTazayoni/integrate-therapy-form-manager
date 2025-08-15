d6c227804a03c4b57f74271344b68b88aaa566e7
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-15 23:49:02 +0100

    Display submitted dates on Becks/Burns score cards

--- src/components/BecksBurnsScoreCard.tsx
+++ src/components/BecksBurnsScoreCard.tsx
@@ -1,13 +1,23 @@
 type ScoreCardProps = {
   title: string;
-  value: string | number;
+  value: string;
+  submittedAt?: string;
 };
 
-export default function ScoreCard({ title, value }: ScoreCardProps) {
+export default function ScoreCard({
+  title,
+  value,
+  submittedAt,
+}: ScoreCardProps) {
   return (
     <div className="flex-1 min-w-[140px] p-6 rounded-md shadow-lg text-center border-2 border-gray-300">
       <h3 className="text-lg font-semibold text-[--color-link] mb-2">
-        {title}
+        {title}{" "}
+        {submittedAt && (
+          <span className="text-gray-400 text-md">
+            ({new Date(submittedAt).toLocaleDateString()})
+          </span>
+        )}
       </h3>
       <p className="text-3xl font-bold text-[--color-secondary] mb-1">
         {value}
