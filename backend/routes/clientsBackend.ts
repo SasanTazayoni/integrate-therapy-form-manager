40b0e1681ac5620e4aa3fc7f1bea9bb647573bea
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-17 14:00:25 +0100

    Add client deletion route by email

--- backend/routes/clientsBackend.ts
+++ backend/routes/clientsBackend.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   createClientHandler,
   getClientFormsStatusHandler,
+  deleteClientByEmailHandler,
 } from "../controllers/clientsController";
 
 const router = Router();
 
 router.post("/add", createClientHandler);
 router.get("/form-status", getClientFormsStatusHandler);
+router.delete("/by-email", deleteClientByEmailHandler);
 
 export default router;
