bdf68a81c35f8fd0201f4539aca2f26e2450bd2a
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-22 17:58:49 +0100

    Update BecksBurnsScoreCard for responsiveness

--- src/components/BecksBurnsScoreCard.tsx
+++ src/components/BecksBurnsScoreCard.tsx
@@ -13,19 +13,19 @@ export default function ScoreCard({
 }: ScoreCardProps) {
   return (
     <div
-      className={`flex-1 min-w-[140px] p-6 rounded-md shadow-lg text-center border-2 ${
+      className={`score-card md:flex-1 p-6 rounded-md shadow-lg text-center border-2 ${
         highlight ? "bg-yellow-200 border-yellow-400" : "border-gray-300"
       }`}
     >
-      <h3 className="text-lg font-semibold text-[--color-link] mb-2">
+      <h3 className="text-lg font-semibold mb-2">
         {title}{" "}
         {submittedAt && (
-          <span className="text-gray-400 text-md">
+          <span className="text-gray-400">
             ({new Date(submittedAt).toLocaleDateString()})
           </span>
         )}
       </h3>
-      <p className="text-xl font-bold mb-1">{value}</p>
+      <p className="text-lg font-bold mb-1">{value}</p>
     </div>
   );
 }
