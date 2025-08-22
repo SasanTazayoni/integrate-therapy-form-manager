e29697ad6d6e78ce013175fdfddb4afdb5a4150e
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-22 23:40:44 +0100

    Update FormActionButton component to use ripple effect

--- src/components/FormActionButton.tsx
+++ src/components/FormActionButton.tsx
@@ -1,3 +1,5 @@
+import Button from "../components/ui/Button";
+
 type Props = {
   label: string;
   disabled: boolean;
@@ -12,19 +14,8 @@ export default function FormActionButton({
   loading = false,
 }: Props) {
   return (
-    <button
-      type="button"
-      disabled={disabled || loading}
-      onClick={onClick}
-      className={`px-4 py-2 rounded text-white w-[100px] flex justify-center items-center gap-2 ${
-        disabled || loading
-          ? "bg-gray-300 cursor-not-allowed"
-          : "bg-blue-500 hover:bg-blue-600"
-      }`}
-      aria-disabled={disabled || loading}
-      aria-busy={loading || undefined}
-    >
+    <Button onClick={onClick} disabled={disabled || loading} loading={loading}>
       {label}
-    </button>
+    </Button>
   );
 }
