c2274b0239fcbb017b25f9e60297ef769de91cf3
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-08-25 11:36:02 +0100

    Extract SchemaType to YSQBoundaries module

---
+++ backend/utils/YSQBoundariesBackend.ts
@@ -0,0 +1,19 @@
+export type SchemaType =
+  | "ED" // Emotional Deprivation
+  | "AB" // Abandonment
+  | "MA" // Mistrust/Abuse
+  | "SI" // Social Isolation
+  | "DS" // Defectiveness
+  | "FA" // Failure
+  | "DI" // Dependence
+  | "VU" // Vulnerability
+  | "EU" // Enmeshment
+  | "SB" // Subjugation
+  | "SS" // Self Sacrifice
+  | "EI" // Emotional Inhibition
+  | "US" // Unrelenting Standards
+  | "ET" // Entitlement
+  | "IS" // Insufficient Self Control
+  | "AS" // Approval Seeking
+  | "NP" // Negativity/Pessimism
+  | "PU"; // Punitiveness
--- backend/utils/YSQScoreUtils.ts
+++ backend/utils/YSQScoreUtils.ts
@@ -1,22 +1,4 @@
-export type SchemaType =
-  | "ED" // Emotional Deprivation
-  | "AB" // Abandonment
-  | "MA" // Mistrust/Abuse
-  | "SI" // Social Isolation
-  | "DS" // Defectiveness
-  | "FA" // Failure
-  | "DI" // Dependence
-  | "VU" // Vulnerability
-  | "EU" // Enmeshment
-  | "SB" // Subjugation
-  | "SS" // Self Sacrifice
-  | "EI" // Emotional Inhibition
-  | "US" // Unrelenting Standards
-  | "ET" // Entitlement
-  | "IS" // Insufficient Self Control
-  | "AS" // Approval Seeking
-  | "NP" // Negativity/Pessimism
-  | "PU"; // Punitiveness
+import { SchemaType } from "./YSQBoundariesBackend";
 
 export function getScoreCategory(schema: SchemaType, score: number): string {
   const boundaries: Record<SchemaType, [number, number, number]> = {
