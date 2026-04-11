60c75e56dfc25e8f14263df933a366df4b506ec8
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-04-11 22:05:09 +0100

    Parse result once and add missing radix argument

--- backend/utils/scoreUtils.ts
+++ backend/utils/scoreUtils.ts
@@ -2,7 +2,8 @@ export function parseAndCombineScore(
   result: string,
   getCategoryFn: (score: number) => string
 ): string {
-  const score = Number.isInteger(parseInt(result)) ? parseInt(result) : 0;
+  const parsed = parseInt(result, 10);
+  const score = Number.isInteger(parsed) ? parsed : 0;
   const category = getCategoryFn(score);
   return `${score}-${category}`;
 }
