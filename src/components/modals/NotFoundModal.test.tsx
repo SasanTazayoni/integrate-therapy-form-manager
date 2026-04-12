e61ff83aef82dbcaf8ed1203fd7f913391dc79ae
Sasan Tazayoni <sasantazayoni@gmail.com>
2026-04-12 17:53:30 +0100

    Convert NotFoundModal.test to TypeScript

--- src/components/modals/NotFoundModal.test.tsx
+++ src/components/modals/NotFoundModal.test.tsx
@@ -19,16 +19,16 @@ afterEach(() => {
 describe("NotFoundModal", () => {
   test("renders default title and message and focuses heading", () => {
     const { container } = render(<NotFoundModal />, {
-      container: document.getElementById("modal-root"),
+      container: document.getElementById("modal-root")!,
     });
 
     const heading = container.querySelector("#not-found-title");
     expect(heading).toBeTruthy();
-    expect(heading.textContent).toBe("This page does not exist");
+    expect(heading!.textContent).toBe("This page does not exist");
 
     const description = container.querySelector("#not-found-desc");
     expect(description).toBeTruthy();
-    expect(description.textContent).toBe(
+    expect(description!.textContent).toBe(
       "You seemed to have navigated to an unknown page. Please check your email for a valid form link."
     );
 
@@ -42,17 +42,17 @@ describe("NotFoundModal", () => {
     const { container } = render(
       <NotFoundModal title={customTitle} message={customMessage} />,
       {
-        container: document.getElementById("modal-root"),
+        container: document.getElementById("modal-root")!,
       }
     );
 
     const heading = container.querySelector("#not-found-title");
     expect(heading).toBeTruthy();
-    expect(heading.textContent).toBe(customTitle);
+    expect(heading!.textContent).toBe(customTitle);
 
     const description = container.querySelector("#not-found-desc");
     expect(description).toBeTruthy();
-    expect(description.textContent).toBe(customMessage);
+    expect(description!.textContent).toBe(customMessage);
 
     expect(document.activeElement).toBe(heading);
   });
